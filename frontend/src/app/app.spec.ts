05e76b9c782c042f6c958e60eed700b49e57f12a
GitYash-2005 <thapayashaws.20@gmail.com>
2026-08-07 11:36:46 +0530

    Finalised

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -32,6 +32,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.brand strong')?.textContent).toContain('Stock Manager');
+    expect(compiled.querySelector('.brand strong')?.textContent).toContain('STOCK MANAGER');
   });
 });
